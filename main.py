04dec63dd52cbc2576cd720a82f0b259ec539fbe
root <michelefalzone@gmail.com>
2016-12-06 19:49:30 +0000

    Upgrade

--- main.py
+++ main.py
@@ -39,35 +39,71 @@ def getTemperature(bot,update):
 	humidity,temperature=Adafruit_DHT.read_retry(11,4)	
 	update.message.reply_text("The temperature is around " + str(temperature) + " C")
 
+def getAutoBit():
+	if(os.path.isfile('/tmp/autobit')):
+		fp = open('/tmp/autobit','r')
+		if(fp.read()=='1'):
+			returnValue="Auto"
+		else: 
+			returnValue="Manual"
+		fp.close()
+		return returnValue
+	else:
+		fp = open('/tmp/autobit','a').close()
+		return "Manual"
+	
+def setAutoBit(bit):
+	if(os.path.isfile('/tmp/autobit')):
+		fp = open('/tmp/autobit','w')
+	else:
+		fp = open('/tmp/autobit','a')
+	
+	fp.write(str(bit))
+	fp.close()
+
+def isAuthorized(bot,update):
+	id_user = update.message.from_user.id
+	if(int(id_user) != int(os.environ['mymaker'])):
+		return False
+	return True
+	
 def getStatus(bot,update):
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
 	response = ""
 	id_user = update.message.from_user.id
 	print int(id_user)
-	print os.environ['mymaker']
-	
-	if(int(id_user) != int(os.environ['mymaker'])):
-		update.message.reply_text("I'm sorry but I answer only to my maker")
+	if(isAuthorized(bot,update) == False):
+		response = "I'm sorry but I can answer only to my maker"	
 	else:
 		# check relay status
 		GPIO.setup(18,GPIO.IN)
 		relay_status=GPIO.input(18)			
+
+		response += "Mode: " + str(getAutoBit()) + "\n"
 		
-		fp = open('~/autobit','r')
-		if(fp.read() == int(1)):
-			response+="Mode: Auto"
-		else:
-			response+="Mode: Manual"
-			
-		fp.close()
-	
 		if(relay_status == 1):
 			response+="The relay is ON, Sir"
 		else:
 			response+="The relay is OFF, Sir"
 		update.message.reply_text(response)
-	
+
+def setStatus(bot,update):
+	if(isAuthorized(bot,update) == False):
+		update.message.reply_text("YOU SHALL NOT PASS!")
+		return 0
+	command = "" + update.message.text
+	command = str(command[11:])
+	command = command.upper()
+	# print len(command) , " - " , command
+	if(len(command) > 0):
+		if(command == "AUTO"):
+			setAutoBit(1)
+			update.message.reply_text("Mode setted to AUTO")
+		elif(command == "MANUAL"):
+			setAutoBit(0)
+			update.message.reply_text("Mode setted to MANUAL")
+
 def notWorksYet(bot,update):
 	update.message.reply_text("I'm sorry but this feature is not yet in production")
 
@@ -85,13 +121,8 @@ def main():
 	dp.add_handler(CommandHandler("",command_list))
 	dp.add_handler(CommandHandler("getTemperature",getTemperature))
 	dp.add_handler(CommandHandler("getStatus",getStatus))
+	dp.add_handler(CommandHandler("setStatus",setStatus))
 	
-	
-		
-	# on noncommand i.e message - echo the message on Telegram
-
-	# dp.add_handler(MessageHandler(Filters.text, echo ))
-
 	# log all errors
 	dp.add_error_handler(error)
 
