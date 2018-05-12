fea993a119f6ca84d527639957f021f4d7a123d9
Michele Falzone <michelfalzone@gmail.com>
2018-05-12 14:55:34 +0200

    wrong directory for crontab

--- dummy-deploy.sh
+++ dummy-deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+pushd /home/pi/workspace/sepphobot
 git fetch origin master
 
 local_commit=$(git rev-list --branches --max-count=1)
@@ -12,3 +13,5 @@ if [[ ${local_commit} != ${remote_commit} ]] ; then
 else
     echo "up to date with remote"
 fi
+
+popd
