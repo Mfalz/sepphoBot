eda3738cf9af3fb6f4fbeafce249453b24c3fd91
Michele Falzone <michelfalzone@gmail.com>
2018-05-12 14:28:19 +0200

    service restart sepphobot

--- dummy-deploy.sh
+++ dummy-deploy.sh
@@ -8,6 +8,7 @@ remote_commit=$(git rev-list --remotes --max-count=1)
 if [[ ${local_commit} != ${remote_commit} ]] ; then
     echo "remote has some new commits"
     git pull
+    sudo systemctl restart sepphobot
 else
     echo "up to date with remote"
 fi
