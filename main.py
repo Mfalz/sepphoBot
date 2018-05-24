21e0a6a20c37df7767a4e03021e800f0ae52c3c9
Michele Falzone <michelfalzone@gmail.com>
2018-05-24 19:56:51 +0200

    db -> dp

--- main.py
+++ main.py
@@ -182,8 +182,7 @@ def main():
     dp.add_handler(start_menu_conv_handler)
 
     for command in commands:
-        global db
-        db.add_handler(CommandHandler(command, command["cmd"]))
+        dp.add_handler(CommandHandler(command, command["cmd"]))
 
     # log all errors
     dp.add_error_handler(error)
